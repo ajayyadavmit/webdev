0c80b88b618476cf965ba0ec39d78ca4675dfffd
ajayyadav <ajayyadav.mit@gmail.com>
2026-06-25 09:55:52 +0545

    web dev

---
+++ script.js
@@ -0,0 +1,19 @@
+let arr = [4, 5, "ram"]
+arr
+
+for (const element of arr) {
+    console.log(element)
+}
+
+const name = "Ajay";
+const age = 30;
+
+const button = document.getElementById('mybutton');
+
+// Add an event listener for the 'click' event
+button.addEventListener('click', function () {
+    alert('hello');
+});
+console.log("Inside JS")
+console.log("Method calc")
+
